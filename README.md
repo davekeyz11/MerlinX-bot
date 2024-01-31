ef45833fbaba6981f2c8f293c91d8d9f53ff6ad0
davekeyz11 <tech4kidsdave@gmail.com>
2024-01-31 15:17:04 -0800

    Added readme file and added code

---
+++ README.md
@@ -0,0 +1,15 @@
+# Chatbot.py
+
+The Chatbot.py project was created by the genius developer David Dyenaan David. It was created through the attempt of teaching people how to type perfectly using capital letters where they are meant to, correct spellings etc.
+
+## Installment
+
+To get the code running you need to follow the following steps:
+
+1. The code will prompt the user to enter a message and you have to enter one of these greetings "hi", "hello", "hey", "good morning", "good afternoon", "good evening" if not the code will print Invalid Input and it will exit the program.
+
+2. If answered correctly the code will also prompt the user to type your name and any name is acceptable in that case.
+
+3. After that it should ask if you like sports and it requires a reply of Yes or No starting with a capital letter but if not it will print invalid code and exit the program.
+
+4. You are allowed to enter any hobby when prompted
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+import sys
 greetings = ["hi", "hello", "hey", "good morning", "good afternoon", "good evening"]
 mergedGreetings = ", ".join(greetings)
 
 user_input = input("Enter your message: ")
 
 if user_input in greetings:
-    print("Greetings detected")
-
+    print("Greeting detected")
+else:
+     print("Invalid Input")
+     sys.exit()
 userName = input("What is your name?: ")
 print ("Welcome " + userName + " I am here to ask you a few questions about yourself")
 user_input2 = input("Do you like sports?: ")
@@ -15,9 +18,13 @@ if user_input2 == "Yes":
 elif user_input2 == "No":
     print("That's okay")
 
+else:
+     print("Invalid input")
+     sys.exit()
 hobbies = ["Playing Piano", "Playing Guitar", "Drawing", "Coding", "Content creating", "Creating 3D Models", "Drawing and animating items"]
 mergedHobbies = ", ".join(hobbies)
 userInput3 = "What is your hobby?: "
 if userInput3 in hobbies:
         print("I also like " + hobbies)
-        
+else:
+     print("That's okay")
